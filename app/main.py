be59328c53b217bc52ce144cbdb52ddeceed0c71
Nicolas Monroy <nmonroy97@gmail.com>
2025-12-03 23:10:36 -0500

    fix: Update CORS middleware to use settings variable for allowed origins

--- app/main.py
+++ app/main.py
@@ -41,7 +41,7 @@ app = FastAPI(
 # Configurar CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=settings.cors_origins_list,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
